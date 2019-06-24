a92ea4dcba74b82b3093a47199e5149518bc5710
Ivan Boryskin <Ivan.Boryskin@portaone.com>
2019-06-24 16:56:36 +0300

    mongodb.sh and ruby.sh renamed, startup_script.sh modified

--- startup_script.sh
+++ startup_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LOGFILE=~/reddit_inst.log
+LOGFILE=/var/log/reddit_inst.log
 DateStr=$(date +"%Y-%m-%d %H:%M:%S")
 
 ##############################################################################
@@ -42,6 +42,7 @@ echo -e "$(sudo systemctl status mongod | egrep -i "load|active")\n$DateStr Mong
 
 echo "$DateStr Reddit deploying starts:" >> $LOGFILE 2>&1
 
+cd /home/appuser/
 git clone -b monolith https://github.com/express42/reddit.git
 
 # install app's dependencies
@@ -52,18 +53,19 @@ else echo "$DateStr -- installation failed. Error code: $? "  >> $LOGFILE 2>&1
 fi
 
 sudo gem install bundler
+if [[ $? -eq 0 ]]; then echo "$DateStr -bundler- installed successfully "  >> $LOGFILE 2>&1
+else echo "$DateStr -- installation failed. Error code: $? "  >> $LOGFILE 2>&1
+fi
 
 # run the app's  server
 puma -d
 
 # checking port app running on
-stps -aux |grep puma |tee -a $LOGFILE 2&>1
-
 _st=ps -aux |grep puma | grep -v grep | awk -F: '/tcp/{print $5}' | egrep -o "^[0-9]{1,5}"
 
 # if [ -z $_st ];  then echo "Reddit is not running"; else echo "Reddit server is running on $_st port"; fi
 
-if [ ! -z $_st ] then echo "$DateStr Reddit Deploying completed. Server is running on $_st port" >> $LOGFILE 2&>1
+if [ ! -z $_st ]; then echo "$DateStr Reddit Deploying completed. Server is running on $_st port" >> $LOGFILE 2&>1
 else
 echo "Reddit server startup failed. For details see log $LOGFILE" >> $LOGFILE 2&>1
 fi
