e717773a7a3e9dff0cc0277d6b9611b6b048d239
Ivan Boryskin <Ivan.Boryskin@portaone.com>
2019-07-03 21:12:44 +0300

    main.tf - main terraform config file was created in terraform folder

---
+++ terraform/maint.tf
@@ -0,0 +1,11 @@
+terraform {
+# Версия terraform
+required_version = "0.11.11"
+}
+provider "google" {
+# Версия провайдера
+version = "2.0.0"
+# ID проекта
+project = "infra-244305"
+region = "us-central1"
+}
