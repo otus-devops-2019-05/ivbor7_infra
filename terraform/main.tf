e2ccd28ca43e642260f2888329b07797a592cf6e
Ivan Boryskin <Ivan.Boryskin@portaone.com>
2019-07-08 23:19:27 +0300

    main.tf, outputs.tf, terraform.tfvars.example, variables.tf - modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,75 +6,85 @@ terraform {
 provider "google" {
   # provider version
   version = "2.0.0"
- # ID project
+
+  # ID project
   project = "${var.project}"
-  region = "${var.region}"
+  region  = "${var.region}"
 }
+
+resource "google_compute_project_metadata_item" "app_multiuserssh" {
+  key = "ssh-keys"
+  value = "${var.ssh_user}:${file(var.public_key_path)} \n${var.ssh_user1}:${file(var.public_key_path)} \n${var.ssh_user2}:${file(var.public_key_path)}"
+}
+
 
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
   machine_type = "f1-micro"
-  zone = "us-central1-a"
-  tags = ["reddit-app"]
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
+# specify the number of instances
+  count        = "${var.count_instance}"
+# and change the name of instance appropriatly
+  name	       = "reddit-app-${count.index}"
 
-  metadata {
- # path to ssh public key
-  ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
-}
+#  metadata {
+# path to ssh public key using the ssh-keys directive in instance is depricated:
+# ssh-keys = "appuser:${file(var.public_key_path)}"
+# ssh-keys = "${var.ssh_user}:${file(var.public_key_path)} \n${var.ssh_user1}:${file(var.public_key_path)} \n${var.ssh_user2}:${file(var.public_key_path)}"
+#  }
 
-# boot disk determination
+
+  # boot disk declaration
   boot_disk {
     initialize_params {
       image = "reddit-base"
     }
-}
+  }
 
-# define the network interface
-network_interface {
+  # define the network interface
+  network_interface {
+    # what network connect to
+    network = "default"
 
-# what network connect to
-network = "default"
-# use  ephemeral IP to access the Internet
-access_config {}
-}
+    # use  ephemeral IP to access the Internet
+    access_config {}
+  }
 
-connection {
-type = "ssh"
-user = "appuser"
-agent = false
-# private key path
-#private_key = "${file("~/.ssh/appuser")}"
-private_key = "${file(var.private_key_path)}"
-}
+  connection {
+    type  = "ssh"
+    user  = "appuser"
+    agent = false
 
+    # private key path
+    #private_key = "${file("~/.ssh/appuser")}"
+    private_key = "${file(var.private_key_path)}"
+  }
 
-provisioner "file" {
-  source = "files/puma.service"
-  destination = "/tmp/puma.service"
-}
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
 
-provisioner "remote-exec" {
-  script = "files/deploy.sh"
-}
-
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
 
 resource "google_compute_firewall" "firewall_puma" {
   name = "allow-puma-default"
 
-# network name where the firewall rule going be acting
+  # network name where the firewall rule going be acting
   network = "default"
 
-# what access type allow
+  # what access type allow
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
 
-# what IP-addresses will be allowed
+  # what IP-addresses will be allowed
   source_ranges = ["0.0.0.0/0"]
 
-# The rule will be applied to instances with listed below tags
+  # The rule will be applied to instances with listed below tags
   target_tags = ["reddit-app"]
-
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,26 +1,51 @@
 variable project {
   description = "Project ID"
-  default = "infra-244305"
+
+  #  default = "infra-244305"
 }
 
 variable region {
   description = "Region"
-# default value
+
+  # default value
   default = "us-central1"
 }
 
+variable zone {
+  description = "Zone"
+}
+
+variable ssh_user {
+  description = "First ssh user"
+}
+
+variable ssh_user1 {
+  description = "Second ssh user"
+}
+
+variable ssh_user2 {
+  description = "Third ssh user"
+}
+
 variable public_key_path {
-# variable description
+  # variable description
   description = "Path to the public key used for ssh access"
-  default = "~/.ssh/appuser.pub"
+
+  #  default = "~/.ssh/appuser.pub"
 }
 
 variable disk_image {
   description = "Disk image"
-  default = "reddit-base"
+  default     = "reddit-base"
 }
 
 variable private_key_path {
   description = "Path to ssh private key"
-  default = "~/.ssh/appuser"
+
+  #  default = "~/.ssh/appuser"
+}
+
+variable count_instance {
+  description = "number of instances"
+  default     = 1
 }
