f3f4c1338a4e1e3cb07390cdc81b47472b6fed4c
Ivan Boryskin <Ivan.Boryskin@portaone.com>
2019-07-13 10:51:46 +0300

    app.json, db.json - two templates were created to bake separate images for VM instances

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,1 @@
-output "app_external_ip" {
-  value = "${google_compute_instance.app.*.network_interface.0.access_config.0.nat_ip}"
-}
-
-#output "lb_ip" {
-#  value = "${google_compute_global_address.reddit-lb-ip.address}"
-#}
-
-output "lb_ip" {
-  value = "${google_compute_global_forwarding_rule.reddit-forward.ip_address}"
-}
-
+# Terraform file
