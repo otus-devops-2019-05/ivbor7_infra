58634700ccd5da6d1481588feca43228fb923ec3
Ivan Boryskin <Ivan.Boryskin@portaone.com>
2019-07-14 23:48:33 +0300

    variables.tf added to the terraform root

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable project {
+  description = "Project ID"
+
+  #  default = "infra-244305"
+}
+
+variable region {
+  description = "Region"
+
+  # default value
+  default = "us-central1"
+}
+
