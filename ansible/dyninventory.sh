5a6b6ee9b119250d1fc6c8e59eaa681b81939324
Ivan Boryskin <Ivan.Boryskin@portaone.com>
2019-07-26 13:06:14 +0300

    ansible.cfg and dyninventory.sh were modified

--- ansible/dyninventory.sh
+++ ansible/dyninventory.sh
@@ -18,11 +18,12 @@ function prn_help() {
 
 cd  ../terraform/stage
 
+# this section should be uncommented after installing jq utility on the host, it necessary for extracting the instance's IPs via TF  
 #DB_IP=$(terraform output -json db_ext_ip | jq ".value")
 #APP_IP=$(terraform output -json app_ext_ip | jq ".value")
 
-APP_IP="35.232.138.12"
-DB_IP="34.66.78.42"
+APP_IP="35.232.3.184"
+DB_IP="34.66.253.118"
 #echo "$1"
 #echo "$PROGRAM"
 #echo $AP_PIP
@@ -33,7 +34,7 @@ export APP_IP DB_IP
 case "$1" in
 
    --list)
-#       envsubst < ../../ansible/dyninv.tpl;
+#       envsubst < ../../ansible/dyninv.tpl; <--  as an option can be  uncommented after installing an envsubst utility
        eval "echo \"$(cat ../../ansible/dyninv.tpl)\""
        exit 0;
        ;;
